d97823017b0c2267b860ebac015dc5e278fa1f85
Mihai <mihai.curteanu@yahoo.ro>
2019-05-27 21:35:20 +0300

    basic login setup

--- server/controllers/movies-api.js
+++ server/controllers/movies-api.js
@@ -1,6 +1,8 @@
+import jwt from 'jsonwebtoken';
 import * as log from 'loglevel';
 import Movies from '../models/movies';
 import Users from '../models/users';
+import { secret } from '../jwt';
 
 export const findAllMovies = async (ctx) => {
   ctx.body = await Movies.find().sort({ updatedAt: -1 }).limit(20);
@@ -19,6 +21,11 @@ export const login = async (ctx) => {
   try {
     const users = new Users(loginDetails);
     // check user, login and redirect
+    ctx.status = 200;
+    ctx.body = {
+      token: jwt.sign({ role: 'admin' }, secret),
+      message: 'Successfully logged in!'
+    };
   } catch (e) {
     log.error(e);
     ctx.response.status = 403;
--- server/jwt.js
+++ server/jwt.js
@@ -1,5 +1,5 @@
 import koaJwt from 'koa-jwt';
 
-const secret = process.env.JWT_SECRET || 'jwt-s3cr3t-p@$$w0rd';
+export const secret = process.env.JWT_SECRET || 'jwt-s3cr3t-p@$$w0rd';
 
 export default koaJwt({ secret });
